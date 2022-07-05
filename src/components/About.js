2783d081d1e3e0c26f3e3566d4174af5b6b0ec0b
Anubhav <anubhavchawla02@gmail.com>
2022-07-05 15:06:58 +0530

    About page modified

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,9 @@ export default function About(props) {
   return (
     <div>
         <h4 className='container text-center my-4' style={{color:props.theme==="light"? "black":"white"}}>Text editing app</h4>
-        <ul>
-          <li>About page content</li>
+        <ul style={{color:props.theme==="light"? "black":"white"}}>
+          <li className='my-3'>About page content :- Texted(it) gives you a way to analyse your text quickly and efficiently be it word count or character count</li>
+          <li>Browser compatible :- This word counter software works in any web browser such as Chrome, Firefox, Safari. It suits to count characters in facebook, blogs, books, essays etc.</li>
         </ul>
     </div>
   )
