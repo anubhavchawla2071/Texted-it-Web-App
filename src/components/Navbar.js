8647c1be588db63cfdd092462aabffb6f3cbfce5
Shiti <shitigarg2003@gmail.com>
2022-10-11 15:17:12 +0530

    added italic button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useState } from "react";
+// import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function Navbar(props) {
   const handleDarkMode = () => {};
